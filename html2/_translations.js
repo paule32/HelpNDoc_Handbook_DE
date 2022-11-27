dfdadc01f1cdb12a43ac8ac395f121a98136e803
Jens Kallup <kallup-dev@web.de>
2022-11-27 19:21:07 +0100

    update

---
+++ html2/_translations.js
@@ -0,0 +1,11 @@
+
+function hnd_ut(a){
+a.TRANSLATIONS['Search term too short'] = "Suche-Einschränkung zu kurz";
+a.TRANSLATIONS['No results'] = "Keine Ergebnise";
+a.TRANSLATIONS['Please enter more characters'] = "Geben Sie mehr Zeichen ein";
+a.TRANSLATIONS['Word list not ready yet. Please wait until the word list is fully downloaded'] = "Wort ist noch nicht verfügbar. Bitte warten Sie bis es geladen wurde.";
+a.TRANSLATIONS['Incorrect or corrupt search data. Please check your HelpNDoc template'] = "Falsche oder kaputte Suchdaten. Überprüfen Sie Bitte Ihre HelpNDoc Vorlage.";
+a.TRANSLATIONS['Related topics...'] = "Relevante Themen...";
+a.TRANSLATIONS['Loading...'] = "Lade...";
+a.TRANSLATIONS['Close'] = "Schließen";
+}
